fc0cd0ebea39af36e83f39fd7224a778ca2f13cc
timothyplummer <timtplummer@gmail.com>
2025-08-07 18:21:02 -0400

    make a new change

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. 60c7db39")
+    return HttpResponse("Hello, world. You're at the polls index. And this is a new change: 60c7db39")
